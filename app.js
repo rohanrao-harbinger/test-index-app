d6eb9b6924ad9678db8c793b09bb39552a87596a
rohanrao-harbinger <rohan.rao@harbingergroup.com>
2019-08-31 11:15:47 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const searchJobs = require('./job-index');
 const { crc32 } = require('crc');
 //candidate search
 //to find relevant candidates from external job source
-let externalJobTitle = 'nurse';
-let externalJobTitleCity = 'Atlanta';
-let externalJobTitleState = 'GA';
+let externalJobTitle = 'sales associate';
+let externalJobTitleCity = 'chicago';
+let externalJobTitleState = 'il';
 
 //job search
 //to find relevant jobs based on candidate chat title
@@ -15,26 +15,30 @@ let candidateChatTitleCity = 'Atlanta';
 let candidateChatTitleState = 'GA';
 
 const filtersCandidate = createParamsForFilter(externalJobTitleCity, externalJobTitleState);
-searchCandidate(externalJobTitle, filtersCandidate).then( (candidateResult) => {
-    candidateResult.matches.map( (element) => {
-        if(element.attrs.sourceindex === 1){
-            console.log("chatsessionid===> ",parseInt(element.id / 10));
-        }else{
-            console.log("pdlData===> ",parseInt(element.id / 10)); 
+searchCandidate(externalJobTitle, filtersCandidate).then((candidateResult) => {
+    candidateResult.matches.map((element) => {
+        if (element.attrs.sourceindex === 1) {
+            console.log("chatsessionid===> ", parseInt(element.id / 10),"title---> ", element.attrs.jobtitle,"city---> ", element.attrs.city,"state---> ", element.attrs.state);
+        } else {
+            console.log("pdlId===> ", parseInt(element.id / 10),"title---> ", element.attrs.jobtitle,"city---> ", element.attrs.city,"state---> ", element.attrs.state);
         }
     });
     console.log('------------------------------------------------------------');
 });
 const filtersJob = createParamsForFilter(candidateChatTitleCity, candidateChatTitleState);
-searchJobs(candidateChatTitle, filtersJob).then( (jobsResult) => {
-    jobsResult.map((element) => console.log("jobId====> ",element));
+if (filtersJob) {
+    filtersJob.city = crc32(filtersJob.city);
+    filtersJob.state = crc32(filtersJob.state);
+}
+searchJobs(candidateChatTitle, filtersJob).then((jobsResult) => {
+    jobsResult.map((element) => console.log("jobId====> ", element));
 });
 function createParamsForFilter(city, state) {
     let filters;
     if (city != null && state != null) {
         filters = {
-            city: crc32(city.trim().toLowerCase()),
-            state: crc32(state.trim().toLowerCase())
+            city: city.trim().toLowerCase(),
+            state: state.trim().toLowerCase()
         }
     }
     else {
